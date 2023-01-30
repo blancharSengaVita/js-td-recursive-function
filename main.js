086705b009838c4b9695f9decd4aa5f48374bafc
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-01-30 09:24:26 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+//fonction factorielle
+
+// function factoriel(number){
+//   if (number === 1){
+//     return 1
+//   }
+//   return number * (factoriel(number-1))
+// }
+//
+// console.log(factoriel(6));
+
+
+//fonction de decompte
+//
+// function countdown (number) {
+//   if (number !== 1) {
+//     console.log(number);
+//     countdown(number-1);
+//   } else {
+//     console.log(1);
+//   }
+// }
+//
+// countdown(10);
+
